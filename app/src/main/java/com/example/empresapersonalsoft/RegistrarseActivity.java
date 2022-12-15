6d8384467901e35b798d95cacdf05613595ca005
CristianP50 <cristo>
2022-12-15 12:12:44 -0500

    se finaliza proyecto con todos sus botones funcionales recordar xamp y vscode para abrir el archivo con android studio

--- app/src/main/java/com/example/empresapersonalsoft/RegistrarseActivity.java
+++ app/src/main/java/com/example/empresapersonalsoft/RegistrarseActivity.java
@@ -1,6 +1,8 @@
 package com.example.empresapersonalsoft;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -13,11 +15,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarseActivity extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener{
 
@@ -47,7 +53,7 @@ public class RegistrarseActivity extends AppCompatActivity implements Response.L
             Toast.makeText(this, "Usuario requerido", Toast.LENGTH_SHORT).show();
             jetusuario.requestFocus();
         }else{
-            url = "http://172.16.62.251:80/WebServices/consulta.php?usr='" + usuario;
+            url = "http://172.16.59.117:80/WebServices/consulta.php?usr=" + usuario;
             jrq = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
             rq.add(jrq);
         }
@@ -80,4 +86,152 @@ public class RegistrarseActivity extends AppCompatActivity implements Response.L
         }
 
     }
+
+    public void Adicionar(View view){
+        usuario=jetusuario.getText().toString();
+        nombre=jetnombre.getText().toString();
+        correo=jetcorreo.getText().toString();
+        clave=jetclave.getText().toString();
+        if (usuario.isEmpty() || nombre.isEmpty() || correo.isEmpty() || clave.isEmpty()){
+            Toast.makeText(this, "Todos los datos son requeridos", Toast.LENGTH_SHORT).show();
+            jetusuario.requestFocus();
+        }
+        else{
+            url = "http://172.16.59.117:80/WebServices/registrocorreo.php";
+            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                            Limpiar_campos();
+                            Toast.makeText(getApplicationContext(), "Registro de usuario realizado", Toast.LENGTH_LONG).show();
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "Registro de usuario incorrecto!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("usr",jetusuario.getText().toString().trim());
+                    params.put("nombre", jetnombre.getText().toString().trim());
+                    params.put("correo",jetcorreo.getText().toString().trim());
+                    params.put("clave",jetclave.getText().toString().trim());
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(postRequest);
+        }
+    }
+
+    public void Modificar(View view){
+        usuario=jetusuario.getText().toString();
+        nombre=jetnombre.getText().toString();
+        correo=jetcorreo.getText().toString();
+        clave=jetclave.getText().toString();
+        if (usuario.isEmpty() || nombre.isEmpty() || correo.isEmpty() || clave.isEmpty()){
+            Toast.makeText(this, "Todos los datos son requeridos", Toast.LENGTH_SHORT).show();
+            jetusuario.requestFocus();
+        }
+        else{
+            url = "http://172.16.59.117:80/WebServices/actualiza.php";
+            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                            Limpiar_campos();
+                            Toast.makeText(getApplicationContext(), "Registro de usuario actualizado", Toast.LENGTH_LONG).show();
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "Actualización de usuario incorrecto!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("usr",jetusuario.getText().toString().trim());
+                    params.put("nombre", jetnombre.getText().toString().trim());
+                    params.put("correo",jetcorreo.getText().toString().trim());
+                    params.put("clave",jetclave.getText().toString().trim());
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(postRequest);
+        }
+    }
+
+    public void Anular(View view){
+        usuario=jetusuario.getText().toString();
+        if (usuario.isEmpty()){
+            Toast.makeText(this, "El usuario es requerido", Toast.LENGTH_SHORT).show();
+            jetusuario.requestFocus();
+        }
+        else{
+            url = "http://172.16.59.117:80/WebServices/anula.php";
+            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                            Limpiar_campos();
+                            Toast.makeText(getApplicationContext(), "Registro anulado", Toast.LENGTH_LONG).show();
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "Error al anular el registro", Toast.LENGTH_LONG).show();
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("usr",jetusuario.getText().toString().trim());
+
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(postRequest);
+        }
+    }
+
+
+    public void Limpiar(View view){
+        Limpiar_campos();
+    }
+
+
+    private void Limpiar_campos(){
+        jetusuario.setText("");
+        jetnombre.setText("");
+        jetcorreo.setText("");
+        jetclave.setText("");
+        jetusuario.requestFocus();
+    }
+
+    public void Regresar(View view){
+        Intent intmain=new Intent(this,MainActivity.class);
+        startActivity(intmain);
+    }
+
+
 }
